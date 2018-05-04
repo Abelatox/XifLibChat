1729df71df76071fc978e34bf01b3cefd08538ad
Abelatox <abel54225@gmail.com>
2018-05-04 15:38:04 +0200

    Fitxers

---
+++ src/fichers/MainFitxers.java
@@ -0,0 +1,19 @@
+package fichers;
+import java.io.File;
+import xiflib.XifLib;
+
+public class MainFitxers {
+
+	public static void main(String[] args) {
+		File in = new File("in.txt");
+		File out = new File("out.txt");
+		XifLib xif = new XifLib();
+		xif.xifrar(in, out);
+		System.out.println();
+		xif.xifrar(out, in);
+		System.out.println("\nHolaaa");
+		System.out.println(xif.xifrar("Hola arbol"));
+		System.out.println(xif.xifrar(xif.xifrar("Hola arbol")));
+
+	}
+}
